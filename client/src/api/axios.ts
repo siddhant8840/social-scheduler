3e62c993c9eb02a0af6b0c28c81d79c96c986740
siddhant8840 <146637403+siddhant8840@users.noreply.github.com>
2026-06-30 21:25:23 +0530

    Update axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://social-media-scheduler-q58b.onrender.com"
 })
 
 export default api;
