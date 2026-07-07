0b1d7ae39bc878c19b89c6cdeadbc7d4b84325d0
SIDDHANT SRIVASTAVA <146637403+siddhant8840@users.noreply.github.com>
2026-07-07 09:57:34 +0530

    Update base URL in axios instance configuration

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://social-media-scheduler-q58b.onrender.com/api"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://social-media-scheduler-q58b.onrender.com"
 })
 
 export default api;
